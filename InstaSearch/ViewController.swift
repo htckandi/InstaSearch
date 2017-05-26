a825c9d827516efcc506f98a9e718e4bb231824a
Сергей Табунщиков <hedkandi@imac-sergey.dlink>
2017-05-26 23:23:50 +0300

    Initial Commit

---
+++ InstaSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaSearch
+//
+//  Created by Сергей Табунщиков on 26.05.17.
+//  Copyright © 2017 Sergey Tabunshikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
